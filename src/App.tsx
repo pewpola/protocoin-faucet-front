ef7d62e69c3cfc1dcddfe2c8b25c3165fe04ba3a
Eduardo <eduardojor77@gmail.com>
2024-04-27 18:19:29 -0300

    adding metamask logo

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,27 @@ function App() {
     <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
       <header className="mb-auto">
         <div>
-          <h3 className="float-md-start mb-0">Cover</h3>
+          <h3 className="float-md-start mb-0">ProtoCoin Faucet</h3>
           <nav className="nav nav-masthead justify-content-center float-md-end">
             <a className="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
-            <a className="nav-link fw-bold py-1 px-0" href="#">Features</a>
-            <a className="nav-link fw-bold py-1 px-0" href="#">Contact</a>
+            <a className="nav-link fw-bold py-1 px-0" href="#">About</a>
           </nav>
         </div>
       </header>
 
       <main className="px-3">
-        <h1>Cover your page.</h1>
-        <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+        <h1>Get your ProtoCoins</h1>
+        <p className="lead">Once a day, earn 1.000 coins just connecting your MetaMask below.</p>
         <p className="lead">
-          <a href="#" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+          <a href="#" className="btn btn-lg btn-secondary fw-bold border-white bg-white">
+            <img src="/public/metamask.svg"/>
+            Connect MetaMask
+          </a>
         </p>
       </main>
 
       <footer className="mt-auto text-white-50">
-        <p>Cover template for <a href="https://getbootstrap.com/" className="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" className="text-white">@mdo</a>.</p>
+        <p>Built by pewpola</p>
       </footer>
     </div>
   );
