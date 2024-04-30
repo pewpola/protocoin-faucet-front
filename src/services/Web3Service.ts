6c0a656095b9c0fb4b47c961162b1afc30d78941
Eduardo <eduardojor77@gmail.com>
2024-04-30 15:22:43 -0300

    adding more verification

--- src/services/Web3Service.ts
+++ src/services/Web3Service.ts
@@ -1,10 +1,11 @@
 import Web3 from "web3";
 
 const mint = async() => {
-    if (!window.ethereum) throw new Error(`No MetaMask found!`);
+    if (!window.ethereum) throw new Error("No MetaMask found!");
 
     const web3 = new Web3(window.ethereum);
     const accounts = await web3.eth.requestAccounts();
+    if (!accounts || !accounts.length) throw new Error("No account allowed!");
     alert(accounts[0]);
 };
 
