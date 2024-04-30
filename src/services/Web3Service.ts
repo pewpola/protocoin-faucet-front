46f06ef1fc439295eea993cf7ec169d83192ed2a
Eduardo <eduardojor77@gmail.com>
2024-04-30 15:15:10 -0300

    using web3js now for metamask verification

--- src/services/Web3Service.ts
+++ src/services/Web3Service.ts
@@ -1,9 +1,11 @@
+import Web3 from "web3";
+
 const mint = async() => {
-    if (!window.ethereum) {
-        alert('Usuário não possui Carteira MetaMask')
-    } else {
-        alert('Usuário possui Carteira MetaMask')
-    }
+    if (!window.ethereum) throw new Error(`No MetaMask found!`);
+
+    const web3 = new Web3(window.ethereum);
+    const accounts = await web3.eth.requestAccounts();
+    alert(accounts[0]);
 };
 
 export default mint;
