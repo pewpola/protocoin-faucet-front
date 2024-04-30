7765676708d54dacb51946cebb2135bd7c4eaefd
Eduardo <eduardojor77@gmail.com>
2024-04-30 15:00:46 -0300

    using ethereum prop for metamask wallet verification

--- src/services/Web3Service.ts
+++ src/services/Web3Service.ts
@@ -1,5 +1,9 @@
 const mint = async() => {
-    alert('mint');
+    if (!window.ethereum) {
+        alert('Usuário não possui Carteira MetaMask')
+    } else {
+        alert('Usuário possui Carteira MetaMask')
+    }
 };
 
 export default mint;
