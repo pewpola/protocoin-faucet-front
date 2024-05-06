8f725abd2d4e226d80da0437f56760cb11b5b83c
Eduardo <eduardojor77@gmail.com>
2024-05-05 21:12:40 -0300

    calling mint function from contract ABI

--- src/services/Web3Service.ts
+++ src/services/Web3Service.ts
@@ -6,12 +6,15 @@ const CONTRACT_ADDRESS = `${process.env.REACT_APP_CONTRACT_ADDRESS}`;
 const mint = async() => {
     if (!window.ethereum) throw new Error("No MetaMask found!");
 
-    const web3 = new Web3(window.ethereum);
+    const web3 =  new Web3(window.ethereum);
     const accounts = await web3.eth.requestAccounts();
     if (!accounts || !accounts.length) throw new Error("No account allowed!");
     alert(accounts[0]);
 
-    const contract = new web3.eth.Contract(ABI as AbiItem[], CONTRACT_ADDRESS);
+    const contract = new web3.eth.Contract(ABI as AbiItem[], CONTRACT_ADDRESS, { from: accounts[0] });
+    const tx = await contract.methods.mint().send();
+
+    alert(tx.transactionHash);
 };
 
 export default mint;
